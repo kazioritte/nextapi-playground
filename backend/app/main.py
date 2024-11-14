07347e4ff2d6748c34bafb2a54e8eb2d31f535f5
kikegamisprix <k.ikegami@sprix.jp>
2024-11-14 14:35:29 +0900

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
